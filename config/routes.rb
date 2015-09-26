87846ef733e4e22cd616dd46c27fc04ae0eb2828
Jen J Wong <jenjwong@gmail.com>
2015-09-25 18:28:03 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+
+  resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
