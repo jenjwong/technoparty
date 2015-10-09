714117d391930c2b483825e4d450a92f55847e24
Jen J Wong <jenjwong@gmail.com>
2015-10-09 12:22:13 -0700

    11.3.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
-class StaticPagesController < ApplicationController
+cclass StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
