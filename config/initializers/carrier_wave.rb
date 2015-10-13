bc66f23222e486856b1300a1ca05124675435800
Jen J Wong <jenjwong@gmail.com>
2015-10-13 11:41:27 -0700

    troubleshootingAWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJLVAIOHHF2TXPAAQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAJIWWO2BIWCDHINCA'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['technoparty']
   end
